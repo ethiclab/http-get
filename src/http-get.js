dc3c2ab4c477a2fa0d755e057476fb89c987242a
Montoya Edu <eduardo.montoya@ethiclab.it>
2019-08-09 11:41:14 +0200

    Travis default nodejs version does not support arrow functions.

--- src/http-get.js
+++ src/http-get.js
@@ -1,10 +1,14 @@
 (function() {
   const f = function(url, options) {
-    return new Promise((resolve, reject) => {
+    return new Promise(function (resolve, reject) {
       let request = new XMLHttpRequest()
       request.open('GET', url, true)
-      request.onload = () => resolve(request.responseText)
-      request.onerror = () => reject(request.status, request.statusText)
+      request.onload = function() {
+        resolve(request.responseText)
+      }
+      request.onerror = function() {
+        reject(request.status, request.statusText)
+      }
       if (options) {
         if (options.accept) {
           request.setRequestHeader('accept', options.accept)
